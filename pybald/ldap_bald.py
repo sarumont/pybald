9dcb9112038033b49c8470168e49538a8e589749
Richard Kolkovich <richard@sigil.org>
2013-12-05 17:14:47 -0700

    Allow specifying some or all arguments on bind

--- pybald/ldap_bald.py
+++ pybald/ldap_bald.py
@@ -3,7 +3,7 @@ import os
 import ConfigParser
 import ldap
 
-def connect(profile="default"):
+def connect(server=None, dn=None, pw=None, profile="default"):
 	"""Connects to an LDAP server. Attempts to read configuration from ~/.ldap.cfg. Falls back to 
 	stdin for bind credentials
 
@@ -14,18 +14,21 @@ def connect(profile="default"):
 	config = ConfigParser.ConfigParser()
 	config.read(os.getenv('HOME')+"/.ldap.cfg")
 
-	server = config.get(profile, "server")
 	if not server:
-		server = raw_input("LDAP server: ")
+		server = config.get(profile, "server")
+		if not server:
+			server = raw_input("LDAP server: ")
 
-	dn = config.get(profile, "binddn")
 	if not dn:
-		dn = raw_input("Bind DN: ")
+		dn = config.get(profile, "binddn")
+		if not dn:
+			dn = raw_input("Bind DN: ")
 
-	pw = config.get(profile, "bindpw")
 	if not pw:
-		pw = getpass.getpass("Bind password: ");
-	
+		pw = config.get(profile, "bindpw")
+		if not pw:
+			pw = getpass.getpass("Bind password: ");
+
 	try:
 		conn = ldap.initialize(server)
 		conn.simple_bind_s(dn, pw)
@@ -36,7 +39,6 @@ def connect(profile="default"):
 		print "Unexpected error:", sys.exc_info()[0]
 		print e
 		sys.exit(1)
-	
 	return conn
 
 if __name__ == "__main__":
