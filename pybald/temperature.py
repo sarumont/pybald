18688eb6a757d4a37c4719f0fceb3842bc220bc0
Richard Kolkovich <richard@sigil.org>
2015-02-08 13:27:42 -0700

    add a temp module

---
+++ pybald/temperature.py
@@ -0,0 +1,14 @@
+import re
+import sys
+from error import InvalidInput
+
+def c_to_f(temp):
+    """Converts a Celcius temperature to Farenheit
+
+    :temp: A temperature in Celcius
+    :returns: The equivalent temperature in Farenheit
+    """
+    return temp * 9.0 / 5.0 + 32.0
+
+if __name__ == "__main__":
+    pass
